fb835e91d5293f473b75bf3a831fb43ec052d5e0
DiegoDiaz0827 <diegoardisu@gmail.com>
2025-10-28 18:29:04 -0500

    first commit

---
+++ src/main/java/com/mycompany/happyfeet/HappyFeet.java
@@ -0,0 +1,16 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.happyfeet;
+
+/**
+ *
+ * @author camper
+ */
+public class HappyFeet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
